8e5ffee1a77ffaab3e1f2e194eec16902a7f36ee
ico9ic <Anshshar38@gmail.com>
2026-07-23 11:53:28 +0530

    Initial Commit

---
+++ lab1/hello.js
@@ -0,0 +1,10 @@
+console.log("Ansh Sharma");
+
+//write a function to take two number and return their sum
+
+const sum = (a, b) => {
+  return a + b;
+};
+
+const result = sum(10, 20);
+console.log(result);
