9b9403cd6707e4515907a88d3ea7e540ef383c6a
ico9ic <Anshshar38@gmail.com>
2026-09-10 12:21:11 +0530

    Update

--- lab3/prg6.js
+++ lab3/prg6.js
@@ -1,5 +1,21 @@
+import { createReadStream } from "fs";
 import http from "http";
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+
+    res.write("<h1>Home Page</h1>")
+    res.end(`
+        <a href='/product'>Product</a>
+        <a href="/contact">Contact Us</a>  
+        `);
+  } else if (req.url === "/product") {
+    const stream = createReadStream("./product.html", { encoding: "utf-8" });
+    stream.pipe(res);
+  } else if (req.url === "/contact") {
+    const stream = createReadStream("./contact.html", { encoding: "utf-8" });
+    stream.pipe(res);
+  } else {
+});
 
 server.listen(3000, () => console.log("prg6 is running..."));
