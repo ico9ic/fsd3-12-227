518666f1d355a00a43e49086127feee388a90b8f
ico9ic <Anshshar38@gmail.com>
2026-09-11 15:42:07 +0530

    update

---
+++ lab4/users.js
@@ -0,0 +1,13 @@
+// we use inmemory database
+let users = [
+  {
+    id: 1,
+    name: "John Doe",
+    mob: " 1234567890",
+    email: "dhannu@gmail.com",
+  },
+];
+
+let nextId = 3;
+
+export const getUsers = () => users;
