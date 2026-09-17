22cb77d8b2e53eb2fe60f1b2f24dd34f7036b43b
ico9ic <Anshshar38@gmail.com>
2026-09-17 12:21:20 +0530

    update

--- lab4/users.js
+++ lab4/users.js
@@ -1,13 +1,24 @@
-// we use inmemory database
 let users = [
   {
     id: 1,
     name: "John Doe",
-    mob: " 1234567890",
-    email: "dhannu@gmail.com",
+    mobile: "123-456-7890",
+    email: "john@example.com",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    mobile: "098-765-4321",
+    email: "jane@example.com",
   },
 ];
 
 let nextId = 3;
 
 export const getUsers = () => users;
+
+export const addUser = (user) => {
+  user.id = nextId++;
+  users.push(user);
+  return user;
+};
